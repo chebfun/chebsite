55927629e9c698cb39bb0cf7d41266c054b95112
Hrothgar <hrothgar@maths.ox.ac.uk>
2014-06-12 03:07:08 +0100

    Finished make_chebdoc. Want to commit in case anything terrible happens.

--- docs/make_chebdoc.m
+++ docs/make_chebdoc.m
@@ -1,5 +1,5 @@
-function genchebdoc
-%GENCHEBDOC Generate Markdown documenation for chebfun methods.
+function make_chebdoc
+%MAKE_CHEBDOC   Generate Markdown documenation for chebfun methods.
 %   The doc text is the same as each method's help text.
 
 % TODO: Add links to things in 'See also'.
@@ -78,11 +78,11 @@ functionTemplate = [
     'class_name: "%s"\n' ...
     'function_name: "%s"\n' ...
     'help_text: \n%s\n' ...
+    'function_snip: %s\n' ...
     'qualifiers: %s\n' ...
     'return_type: %s\n' ...
     'arguments: %s\n' ...
     '---\n' ...
-    '\n'
     ];
 
 indexTemplate = [
@@ -91,12 +91,11 @@ indexTemplate = [
     'layout: function-reference\n' ...
     'class_name: "%s"\n' ...
     'function_names: "%s"\n' ...
-    'function_snips: \n%s\n' ...
+    'function_snippet: \n%s\n' ...
     'function_qualifiers: %s\n' ...
     'function_return_type: %s\n' ...
     'function_arguments: %s\n' ...
     '---\n' ...
-    '\n'
     ];
 
 %-----------------------------------------------------------------------------
@@ -105,14 +104,22 @@ indexTemplate = [
 % an index.md file for each class. The main index file is not created here.
 % Note that there is a special case for constructors.
 
+NL     = sprintf('\n');                              % newline character
+toList = @(s) ['- '  strrep(s, NL, [NL '- ' ])];     % codeblock to list
+indent = @(s) ['    ' strrep(s, NL, [NL '    '])];   % indent codeblock
+
 for className = classNames
 
     [attrNames, methodsData] = methodsview(className, 'noUI'); % Undocumented.
     functionNames = methodsData(:, 3);
 
+    %-------------------------------------------------------------------------
+    % First, each function.
+
     for k = 1:length(functionNames)
 
         functionName = functionNames(k);
+        snippets     = {};
 
         % The constructor needs a special thing.
         callToHelp = which([className '/' functionName]);
@@ -121,18 +128,17 @@ for className = classNames
             callToHelp = className;
         end
 
-        % These are the relevant variables.
+        % These are the template variables.
         f_title        = functionName;
         f_className    = className;
         f_functionName = functionName;
-        f_helpText     = help(callToHelp);
+        f_helpText     = indent(help(callToHelp));
+        f_snippet      = getSnippetFrom(f_helpText);
         f_qualifiers   = methodsData(k, 1);
         f_returnType   = methodsData(k, 2);
         f_arguments    = methodsData(k, 4);
 
-        % We have to indent the help text for this to work.
-        NL         = sprintf('\n');   % Newline character.
-        f_helpText = ['    ' strrep(f_helpText, NL, [NL '    '])];
+        snippets{k} = f_snippet;
 
         % Here is the array we'll pass to `fprintf`.
         variables = {f_title,
@@ -149,106 +155,48 @@ for className = classNames
         theMarkdown = sprintf(functionTemplate, variables{:});
         fileHandle  = fopen(fileName, 'w');
         fprintf(fileHandle, theMarkdown);
+        fclose(fileHandle);
     end
 
+    %-------------------------------------------------------------------------
     % Now generate the index file.
 
+    % These are the template variables.
+    f_title         = className;
+    f_className     = className;
+    f_functionNames = indent(toList(char(methodsData(:,3))));
+    f_functionSnips = indent(toList(char(snippets)));
+    f_qualifiers    = indent(toList(char(methodsData(:,1))));
+    f_returnTypes   = indent(toList(char(methodsData(:,2))));
+    f_arguments     = indent(toList(char(methodsData(:,4))));
+
+    % Here is the array we'll pass to `fprintf`.
+    variables = {f_title,
+                 f_className,
+                 f_functionNames,
+                 f_functionSnips,
+                 f_qualifiers,
+                 f_returnTypes,
+                 f_arguments
+                 };
+
+    % Finally, do the templating and write the file.
     fileName    = [className '/index.md'];
-    theMarkdown = sprintf(md_template, variables{:});
+    theMarkdown = sprintf(indexTemplate, variables{:});
     fileHandle  = fopen(fileName, 'w');
     fprintf(fileHandle, theMarkdown);
+    fclose(fileHandle);
 end
 
-
-
-
-
-cd(opts.mdir);
-
-try
-
-    indextext = [indextext '%% Methods for class |chebfun|' NL];
-    indextext = [indextext gendoc4these(fcheb, 'chebfun', opts)];
-    indextext = [indextext '%% Other Chebfun-related methods' NL];
-    indextext = [indextext gendoc4these(fnoncheb, '', opts)];
-
-    % Then generate an index page.
-    disp(['Publishing index file...']);
-
-    indexfile = 'index.m';
-    fh = fopen(indexfile, 'w+');
-    fwrite(fh, indextext);
-    fclose(fh);
-
-    pub.outputDir = ['../' opts.docdir];
-    publish(indexfile, pub);
-    cd('..');
-
-catch err
-
-    cd('..');
-    error(err.identifier)
-
-end
-
-toc
 return
 
-%------------------------------------------------------------
-function indextext = gendoc4these(list, theclass, opts)
-%GENDOC4THESE   Generate and Publish doc files for the methods
-%   of class THECLASS specified in the cell array LIST.
-%
-%   OPTS is a struct with these (required) options:
-%       opts.fsuffix = m-file suffix (to prevent path pollution)
-%       opts.docdir  = directory to put the HTML files in
 
-NL = sprintf('\n'); % newline character
-indextext = [];
+%-----------------------------------------------------------------------------
+function snippet = getSnippetFrom(s)
+%GETSNIPPETFROM   Return this one-line description from some help text.
 
-if ~isempty(theclass),
-    theclass = [theclass '/'];
-end
-
-for k = 1:length(list),
-
-    fname    = list{k};                     % e.g. 'abs'
-    ffull    = [theclass fname];            % e.g. 'chebfun/abs'
-    filename = [fname opts.fsuffix '.m'];   % e.g. 'abs_DOC.m'
-    helptext = help(which(ffull));          % grab helptext of 'chebfun/abs'
-
-    fsnip    = helptext(1:find(helptext==NL)-1);  % First line of help text
-    fsnip    = strrep(fsnip, '   ', ' :: ');      % Punctuation, readability
-    fsnip    = strtrim(fsnip);                    % Remove trailing whitespace
-
-    doctext  = helptext;
-    doctext  = [fsnip doctext(find(doctext==NL,1):end)]; % Formatting
-    doctext  = ['%% ' strrep(doctext, NL, [NL '%'])];    % Remove '%'
-
-    flink    = [fname '.html'];
-
-    % There are some inconsistencies with declared functions,
-    % so this is sort of an existence check.
-    if length(helptext) == 0,
-        continue;
-    end
-
-    disp(['Publishing ' ffull '...']);
-
-    pub.outputDir = ['../' opts.docdir]; % docdir is relative to mdir
-    pub.format = 'html';
-
-    fh = fopen(filename, 'w+');
-    fwrite(fh, doctext);
-    fclose(fh);
-    publish(filename, pub);
-
-    % Rename the HTML file (it doesn't need a suffix).
-    movefile([pub.outputDir '/' fname opts.fsuffix '.html'], ...
-             [pub.outputDir '/' fname '.html']);
-
-    nextline  = ['% * <' flink ' ' fsnip '>' NL];
-    indextext = [indextext nextline];
-end
+firstline = strtrim(s(1,:));
+indexOfFirstSpace = find(firstline == ' ', 1);
+snippet = strtrim(firstline(indexOfFirstSpace+1:end));
 
 return
