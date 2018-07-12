16b3882b8a9a83335f7b8c842c33ca6f1d6b2676
Anthony P. Austin <austina@anl.gov>
2018-07-11 23:22:02 -0700

    Don't interpret exponentiated asterisks in LaTeX formulae as Markdown.
    This commit modifies the regex used by the Markdown interpreter for
    detecting words *emphasized* with asterisks to ignore asterisks that are
    immediately preceded by a caret ('^').  This allows you to write $A^*$
    for the Hermitian adjoint of A without running into trouble.
    
    Note that the default regex actually works most of the time; it's only
    when * is applied to two matrices that appear in the same equation on
    the same line that we run into trouble (e.g., $C = A^* + B^*$ would be
    problematic).
    
    Like the other hack used to modify the behavior of underscore
    _emphasis_, this does NOT actually check that we're in math mode; it'll
    ignore "^*" in text mode too.  This is likely not much of an issue...

--- chebsitemd.py
+++ chebsitemd.py
@@ -5,9 +5,9 @@ Chebfun website extension for Python-Markdown
 Modify the behavior of the Python-Markdown module to tweak some things for the
 Chebfun website.  The following adjustments are currently made:
 
-    - Replace the regex pattern for detecting underscore emphasis to cause less
-      trouble with LaTeX by restricting the characters allowed before and after
-      the underscores.
+    - Replace the regex patterns for detecting asterisk and underscore
+      emphasis to cause less trouble with LaTeX by restricting the characters
+      allowed before and after the underscores.
 
 Written By:  Anthony P. Austin, April 29, 2016.
 """
@@ -19,12 +19,14 @@ from markdown.inlinepatterns import SimpleTagPattern
 
 # This is a modified version of the EMPHASIS_2_RE regex in the
 # inlinepatterns.py module that ships as part of Python-Markdown.
+EMPHASIS_RE = r'(?<![\^])(\*)([^\*]+)(?<![\^])(\*)'
 EMPHASIS_2_RE = r'(?<![\w)}])(_)(?!_)(.+?)(?<!_)\2(?![\w({])'
 
 class ChebsiteExtension(Extension):
     """ Extensions to Markdown for the Chebfun website. """
 
     def extendMarkdown(self, md, md_globals):
+        md.inlinePatterns['emphasis'] = SimpleTagPattern(EMPHASIS_RE, 'em')
         md.inlinePatterns['emphasis2'] = SimpleTagPattern(EMPHASIS_2_RE, 'em')
 
 
