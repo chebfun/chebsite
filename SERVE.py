23729ac685768eeb53b8ff4ad7b93d4e19c963a7
Anthony P. Austin <anthony.austin@nps.edu>
2023-03-23 10:47:29 -0700

    Update SERVE.py for compatibility with Python 3.

--- SERVE.py
+++ SERVE.py
@@ -1,19 +1,27 @@
-
 from __future__ import print_function
 import os
 
+try:
+    # Python 3 HTTP server libraries.
+    from http.server import SimpleHTTPRequestHandler
+    from socketserver import TCPServer
+except ImportError:
+    # Python 2 HTTP server libraries.
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+    from SocketServer import TCPServer
+
 def serve():
     """HTTP server straight from the docs."""
+
     # allow running this from the top level
     if os.path.isdir('_build'):
         os.chdir('_build')
-    import SimpleHTTPServer
-    import SocketServer
+
     # local use, address reuse should be OK
-    SocketServer.TCPServer.allow_reuse_address = True
+    TCPServer.allow_reuse_address = True
+
     PORT = 8000
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-    httpd = SocketServer.TCPServer(('', PORT), Handler)
+    httpd = TCPServer(('', PORT), SimpleHTTPRequestHandler)
     print("Serving at port {}".format(PORT))
     httpd.serve_forever()
 
